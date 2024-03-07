80d7250fba103ca98d7a1cac6c1244e90a263e06
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-07 11:49:37 +0100

    add syncs

--- src/Example.tsx
+++ src/Example.tsx
@@ -214,6 +214,7 @@ const ExampleComponent = () => {
           project_users: true,
           subproject_users: true,
           place_users: true,
+          notifications: true,
         },
       })
       const messagesShape = await db.messages.sync({
